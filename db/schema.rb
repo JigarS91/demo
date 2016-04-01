f528aa4f6aa1f3392be38fe08c41b33eaea0f403
Jigar Shah <JigarS@Jigars-MacBook-Pro.local>
2016-04-01 16:06:53 +0530

    commit

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160331115446) do
+ActiveRecord::Schema.define(version: 20160331175735) do
+
+  create_table "microposts", force: :cascade do |t|
+    t.string   "content",    limit: 255
+    t.integer  "user_id",    limit: 4
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name",       limit: 255
